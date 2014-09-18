82f09e179475de56332f91a4c573300e5e3e6f36
Chen Fisher <chen.fisher@gmail.com>
2014-09-18 14:43:12 +0300

    probe request with channel

--- project.clj
+++ project.clj
@@ -4,6 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/core.async "0.1.338.0-5c5012-alpha"]
                  [http-kit "2.1.16"]
                  [compojure "1.1.9"]]
   :main ^:skip-aot triangulate.core
--- src/triangulate/core.clj
+++ src/triangulate/core.clj
@@ -2,15 +2,28 @@
 	(:require [compojure.core :refer :all]
 						[compojure.route :as route]
 						[compojure.handler :refer [site]]
-						[org.httpkit.server :refer :all])
+						[org.httpkit.server :refer :all]
+						[clojure.core.async :as async :refer :all])
   (:gen-class))
+
+(def probes (chan))
 
 (defn hello [req] {:status 200 :headers {"Content-Type" "application/json; charset=utf-8"} :body "Hello there"})
 
+(defn probe 
+  "gets a probe request and outputs it to a channel to be triangulated"
+  [req]
+  (go (>! probes req))
+  {:status 200 :headers {"Content-Type" "application/json; charset=utf-8"} :body "got probe"})
+
 (defroutes all-routes
   (GET "/" [] hello)
+  (POST "/probe" [] probe)
   (route/files "/") ;; static file url, in `public` folder
   (route/not-found "<p>Page not found.</p>")) ;; all other, return 404
 
 (defn -main [& args]
-	(run-server (site #'all-routes) {:port 8080}))
+	(run-server (site #'all-routes) {:port 8080})
+	(go (while true (println (<! probes))))
+	(println "running..."))
+
