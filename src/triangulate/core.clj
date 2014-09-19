3d10b41646c9eb6c8cf4592c244a1f3d45a8f5b6
Chen Fisher <chen.fisher@gmail.com>
2014-09-19 16:20:31 +0300

    added 'map' endpoint to return json map

--- src/triangulate/core.clj
+++ src/triangulate/core.clj
@@ -3,9 +3,11 @@
 						[compojure.route :as route]
 						[compojure.handler :refer [site]]
 						[org.httpkit.server :refer :all]
-						[ring.middleware.json :refer [wrap-json-params]]
+						[ring.middleware.json :refer [wrap-json-params wrap-json-response]]
 						[clojure.core.async :as async :refer [chan go <! >!]]
-            [triangulate.probes :as probes])
+            [triangulate.probes :as probes]
+            [triangulate.redis :refer (wcar*)]
+            [taoensso.carmine :as car])
   (:gen-class))
 
 (def probes-chan (chan))
@@ -16,16 +18,23 @@
   "gets a probe request and outputs it to a channel to be triangulated"
   [req]
   (go (>! probes-chan req))
-  {:status 200 :headers {"Content-Type" "application/json; charset=utf-8"} :body "got probe"})
+  {:status 204 :headers {}})
+
+(defn get-map 
+  "returns the map"
+  [req]
+  {:status 200 :headers {"Content-Type" "application/json; charset=utf-8"} 
+    :body (apply assoc {} (wcar* (car/hgetall "map")))})
 
 (defroutes all-routes
   (GET "/" [] hello)
-  (ANY "/probe" [] probe)
+  (POST "/probe" [] probe)
+  (GET "/map" [] get-map)
   (route/files "/") ;; static file url, in `public` folder
   (route/not-found "<p>Page not found.</p>")) ;; all other, return 404
 
 (defn -main [& args]
-	(run-server (-> (site #'all-routes) wrap-json-params) {:port 8080})
+	(run-server (-> (site #'all-routes) wrap-json-params wrap-json-response) {:port 8080})
 	(go (while true (probes/handle (<! probes-chan))))
 	(println "running..."))
 
